b6bcaa760bdce1ee5428bdf0ac9c3bcda78770b0
LissaSim <simutina.liza2003@gmail.com>
2024-02-08 12:14:21 +0200

    fix

--- src/components/InfoBlockSection/InfoBlockSection.jsx
+++ src/components/InfoBlockSection/InfoBlockSection.jsx
@@ -1,8 +1,7 @@
 
 import React from 'react';
 import './InfoBlockSection.scss';
-import Comment from '../Comment/Comment';
-import InfoBlock from '../InfoBlock/InfoBlock';
+import InfoBlock from "../InfoBlock/InfoBlock.jsx";
 import Button from '../Button/Button';
 
 
