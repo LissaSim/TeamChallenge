2d28040d0c5b6ace6a3d38b9e299b9fc19ed5e07
Tetiana Fedorenko <1031213@gmail.com>
2024-02-12 11:41:16 +0200

    1

--- src/components/InfoBlockSection/InfoBlockSection.jsx
+++ src/components/InfoBlockSection/InfoBlockSection.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './InfoBlockSection.scss';
-import InfoBlock from "../InfoBlock/InfoBlock.jsx";
+import InfoBlock from "../InfoBlock/InfoBlock";
 import Button from '../Button/Button';
 
 
