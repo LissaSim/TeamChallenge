85f77cc763ca9e3ad226ac905b92068d8662c940
LissaSim <simutina.liza2003@gmail.com>
2024-02-16 10:24:34 +0200

    fix

--- src/components/InfoBlockSection/InfoBlockSection.jsx
+++ src/components/InfoBlockSection/InfoBlockSection.jsx
@@ -1,61 +1,58 @@
 
 import React from 'react';
 import './InfoBlockSection.scss';
-import InfoBlock from "../InfoBlock/InfoBlock.jsx";
+import InfoBlock from "../InfoBlock/InfoBlock";
 import Button from '../Button/Button';
 
-
-
-
 const InfoBlockSection = (props) => {
-  
+
     const button = props.btnName ? <Button content={props.btnName } classStyle='button--line'/> : '';
 
-  return (
-    <section className = {`infoBlockSection ${props.classWrap}`}>
+    return (
+        <section className = {`infoBlockSection ${props.classWrap}`}>
 
-      <div className="infoBlockSection__title-block">
-      {
-        React.Children.map(props.children,(child,index)=>{
-        
+            <div className="infoBlockSection__title-block">
+                {
+                    React.Children.map(props.children,(child,index)=>{
 
-                if(child.type == 'h2'){
-                return child;
+
+                        if(child.type == 'h2'){
+                            return child;
+                        }
+                    })
                 }
-              })
-        }
-        {/* here we add link for going to site */}
-        {props.link ? <a href="#">{props.link}</a> : '' }
-        
-      </div>
-   
-  
- 
-      <div className="infoBlockSection__wrap">
-      {
-        React.Children.map(props.children,(child,index)=>{
+                {/* here we add link for going to site */}
+                {props.link ? <a href="#">{props.link}</a> : '' }
 
-                if(child.type !== 'h2' || child.type == 'button' ){
-                return (
-                    <InfoBlock>
-                 {child}
-                     </InfoBlock>
-                )
-           
+            </div>
+
+
+
+            <div className="infoBlockSection__wrap">
+                {
+                    React.Children.map(props.children,(child,index)=>{
+
+                        if(child.type !== 'h2' || child.type == 'button' ){
+                            return (
+                                <InfoBlock>
+                                    {child}
+                                </InfoBlock>
+                            )
+
+                        }
+                    })
                 }
-              })
-        }
-      {/* <InfoBlock>
+                {/* <InfoBlock>
       <Comment/>
       </InfoBlock> */}
 
- 
 
-      </div>
-      {button}
 
-     
-    </section>
-  )
+            </div>
+            {button}
+
+
+        </section>
+    )
 }
 export default InfoBlockSection
