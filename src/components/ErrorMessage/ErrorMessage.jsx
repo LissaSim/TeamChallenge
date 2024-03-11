dc42f87f6ce17f3a7f52fbba5df8a34bb311ad07
LissaSim <simutina.liza2003@gmail.com>
2024-03-11 08:58:39 +0200

    fix: update func pagination,update design

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,4 +1,5 @@
-import img from './error.gif'
+import img from './error.gif';
+import './ErrorMessage.scss'
 const ErrorMessage = () => {
     return (
         <img src={img} alt="Error" style={{display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto'}}/>
