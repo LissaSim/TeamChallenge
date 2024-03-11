c1825c8563b229341a5701303a2b932c54b8c428
LissaSim <simutina.liza2003@gmail.com>
2024-03-11 09:05:06 +0200

    fix: register

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -1,5 +1,4 @@
 import img from './error.gif';
-import './ErrorMessage.scss'
 const ErrorMessage = () => {
     return (
         <img src={img} alt="Error" style={{display: 'block', width: '250px', height: '250px', objectFit: 'contain', margin: '0 auto'}}/>
